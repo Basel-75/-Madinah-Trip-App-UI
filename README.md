f8e115dbea921f2e99706b960acd846cf160e870
Basel-75 <basel_1422@outlook.com>
2024-01-11 03:12:57 +0300

    upadte

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
-# tripapp
+# Trip App - Explore Famous Locations in Medina, Saudi Arabia
 
-A new Flutter project.
+A Flutter application showcasing the most famous locations in Medina, Saudi Arabia. This app uses a PageView to present detailed information about each location along with captivating images.
 
-## Getting Started
+## Features:
 
-This project is a starting point for a Flutter application.
+- Interactive PageView to navigate through famous locations.
+- Rich content for each location, including images, titles, and descriptions.
+- Responsive design for an optimal viewing experience on various devices.
 
-A few resources to get you started if this is your first Flutter project:
+## Screenshots:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+![Masjid al Nabawi](assets/images/one.jpg)
+![Masjid e Quba](assets/images/two.jpg)
+![Masjid Qiblatain](assets/images/three.jpg)
+![King Fahd Complex](assets/images/four.jpg)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Usage:
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/Basel-75/trip_app.git
+   cd trip_app
+Open the project:
+Open the project in your preferred Flutter development environment (e.g., VS Code, Android Studio).
+
+Run the app:
+Run the app on an emulator or physical device.
+
+Dependencies:
+Flutter
+Author:
+Basel-75
+
+Feel free to contribute, report issues, or suggest enhancements!
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ));
+void main() => runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -16,14 +16,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late PageController _controller;
-  void _onScroll() {}
 
   @override
   void initState() {
     super.initState();
     _controller = PageController(
       initialPage: 0,
-    )..addListener(_onScroll);
+    );
   }
 
   @override
@@ -33,68 +32,75 @@ class _HomePageState extends State<HomePage> {
         controller: _controller,
         children: [
           makePage(
-              NO: '1',
-              image: 'assets/images/one.jpg',
-              title: 'masjid al nabawi',
-              description:
-                  "The Prophet's Mosque or Mosque of the Prophet (Arabic: ٱلْمَسْجِد ٱلنَّبَوِي‎, romanized: al-Masjid an-Nabawī, lit. 'Mosque of the Prophet'), is the second mosque built by the Islamic prophet Muhammad in Medina, after that of Quba, as well as the second largest mosque and holiest site in Islam"),
+            NO: '1',
+            image: 'assets/images/one.jpg',
+            title: 'Masjid al Nabawi',
+            description:
+                "The Prophet's Mosque or Mosque of the Prophet (Arabic: ٱلْمَسْجِد ٱلنَّبَوِي‎) is the second mosque built by the Islamic prophet Muhammad in Medina.",
+          ),
           makePage(
-              NO: '2',
-              image: 'assets/images/tow.jpg',
-              title: 'Masjid E Quba',
-              description:
-                  "The Quba Mosque (Arabic: مَسْجِد قُبَاء‎, romanized: Masjid Qubāʾ) is a mosque located in Medina, in the Hejazi region of Saudi Arabia, built in the lifetime of the Islamic prophet Muhammad in the 7th century C.E. It is thought to be the first mosque in the world, built on the first day of Muhammad's emigration to Medina."),
+            NO: '2',
+            image: 'assets/images/tow.jpg',
+            title: 'Masjid e Quba',
+            description:
+                "The Quba Mosque (Arabic: مَسْجِد قُبَاء‎) is a mosque located in Medina, in the Hejazi region of Saudi Arabia, built in the lifetime of the Islamic prophet Muhammad.",
+          ),
           makePage(
-              NO: '3',
-              image: 'assets/images/three.jpg',
-              title: 'Masjid Qiblatain',
-              description:
-                  "The Masjid al-Qiblatayn (Arabic: مسجد القبلتين, lit. 'Mosque of the Two Qiblas'), also spelt Masjid al-Qiblatain, is a mosque in Medina believed by Muslims to be the place where the final Islamic prophet, Muhammad, received the command to change the Qibla (direction of prayer) from Jerusalem to Mecca."),
+            NO: '3',
+            image: 'assets/images/three.jpg',
+            title: 'Masjid Qiblatain',
+            description:
+                "The Masjid al-Qiblatayn is a mosque in Medina believed by Muslims to be the place where the final Islamic prophet, Muhammad, received the command to change the Qibla.",
+          ),
           makePage(
-              NO: '4',
-              image: 'assets/images/four.jpg',
-              title: 'King Fahd Complex',
-              description:
-                  'King Fahd Complex for the Printing of the Holy Qur’an, in Medina, is the largest printing press in the world for printing the Qur’an. It is one of the landmarks provided by the Kingdom of Saudi Arabia to serve Islam and Muslims in various parts of the world. The Custodian of the Two Holy Mosques, King Fahd bin Abdulaziz, laid the foundation stone for the complex in 1403 AH and inaugurated it in 1405 AH.'),
+            NO: '4',
+            image: 'assets/images/four.jpg',
+            title: 'King Fahd Complex',
+            description:
+                'King Fahd Complex for the Printing of the Holy Qur’an, in Medina, is the largest printing press in the world for printing the Qur’an.',
+          ),
         ],
       ),
     );
   }
 
-  Widget makePage({image, title, description, NO}) {
+  Widget makePage(
+      {String? image, String? title, String? description, String? NO}) {
     return Container(
       decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+        image: DecorationImage(
+          image: AssetImage(image ?? ''),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.bottomRight,
-                stops: const [
-              0.3,
-              0.9
-            ],
-                colors: [
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: const [0.3, 0.9],
+            colors: [
               Colors.black.withOpacity(.9),
-              Colors.black.withOpacity(.2)
-            ])),
+              Colors.black.withOpacity(.2),
+            ],
+          ),
+        ),
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              const SizedBox(
-                height: 40,
-              ),
+              const SizedBox(height: 40),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.baseline,
                 textBaseline: TextBaseline.alphabetic,
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Text(
-                    NO,
+                    NO ?? '',
                     style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                   const Text(
                     '/4',
@@ -106,92 +112,60 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               Expanded(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title ?? '',
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 50,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(right: 3),
-                        child: const Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        for (int i = 0; i < 5; i++)
+                          Container(
+                            margin: const EdgeInsets.only(right: 3),
+                            child: const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                        Text(
+                          '5.0',
+                          style: const TextStyle(color: Colors.white70),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(right: 3),
-                        child: const Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
+                        const Text(
+                          ' (22675)',
+                          style: TextStyle(color: Colors.white38, fontSize: 12),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(right: 3),
-                        child: const Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(right: 3),
-                        child: const Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(right: 5),
-                        child: const Icon(
-                          Icons.star,
-                          color: Colors.yellow,
-                          size: 15,
-                        ),
-                      ),
-                      const Text(
-                        '5.0',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                      const Text(
-                        ' (22675)',
-                        style: TextStyle(color: Colors.white38, fontSize: 12),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    description,
-                    style: TextStyle(
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      description ?? '',
+                      style: TextStyle(
                         color: Colors.white.withOpacity(.8),
                         height: 1.9,
-                        fontSize: 15),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    'Read More',
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ))
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Read More',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
